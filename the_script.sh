8e0047718b488bef33be283b1b7d5378ba12c573
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2023-04-03 23:46:59 +0800

    feat: draw complex table that contains thumbnails and more

--- the_script.sh
+++ the_script.sh
@@ -19,51 +19,120 @@ file "${channel_list_json}" >/dev/stderr
 now_second=$(date '+%s');
 limit_second=$((${now_second} + ${offset_second}));
 
-echo '| START (UTC) | URL | ".video.allow_dvr_flg" | ".video.convert_to_vod_flg" |';
-echo '| - |:- |:-:|:-:|';
 
-<"${channel_list_json}" jq --compact-output '.data.content_providers | .[]' | \
-  while read -r channel_info; do
-    fanclub_site_id="$(jq --raw-output '.id' <<<"${channel_info}")";
-    domain="$(jq --raw-output '.domain' <<<"${channel_info}")";
+# collect live
 
-    live_page_info="$(
-      curl -sS \
-        -H 'fc_use_device: null' \
-        "https://nfc-api.nicochannel.jp/fc/fanclub_sites/${fanclub_site_id}/live_pages?page=1&live_type=2&per_page=1" | \
-      jq '.data' \
-    )";
+declare -A live_timestamp_code_row_map
 
-    if [[ "${live_page_info}" != 'null' ]]; then
-      live_list="$(jq '.video_pages.list' <<<"${live_page_info}")";
+while read -r channel_info; do
+  fanclub_site_id="$(jq --raw-output '.id' <<<"${channel_info}")";
+  domain="$(jq --raw-output '.domain' <<<"${channel_info}")";
 
-      if [[ "${live_list}" != '[]' ]]; then
-        content_code="$(jq --raw-output '.[0].content_code' <<<"${live_list}")";
+  live_page_info="$(
+    curl -sS \
+      -H 'fc_use_device: null' \
+      "https://nfc-api.nicochannel.jp/fc/fanclub_sites/${fanclub_site_id}/live_pages?page=1&live_type=2&per_page=1" | \
+    jq '.data' \
+  )";
 
-        live_info="$(
-          curl -sS \
-            -H 'fc_use_device: null' \
-            "https://nfc-api.nicochannel.jp/fc/video_pages/${content_code}" | \
-          jq '.data.video_page' \
-        )";
+  if [[ "${live_page_info}" != 'null' ]]; then
+    live_list="$(jq '.video_pages.list' <<<"${live_page_info}")";
 
-        live_scheduled_start_at="$(jq --raw-output '.live_scheduled_start_at' <<<"${live_info}")";
+    if [[ "${live_list}" != '[]' ]]; then
+      content_code="$(jq --raw-output '.[0].content_code' <<<"${live_list}")";
 
-        video_allow_dvr_flg="$(jq --raw-output '.video.allow_dvr_flg' <<<"${live_info}")";
-        [[ "${video_allow_dvr_flg}" == 'true' ]] && video_allow_dvr_flg='';
+      echo "processing [${content_code}]" >/dev/stderr
 
-        video_convert_to_vod_flg="$(jq --raw-output '.video.convert_to_vod_flg' <<<"${live_info}")";
-        [[ "${video_convert_to_vod_flg}" == 'true' ]] && video_convert_to_vod_flg='';
+      live_info="$(
+        curl -sS \
+          -H 'fc_use_device: null' \
+          "https://nfc-api.nicochannel.jp/fc/video_pages/${content_code}" | \
+        jq '.data.video_page' \
+      )";
 
-        live_scheduled_start_at_second=$(date --date="${live_scheduled_start_at}" '+%s');
+      live_scheduled_start_at="$(jq --raw-output '.live_scheduled_start_at' <<<"${live_info}")";
 
-        if [[ ${now_second} -le ${live_scheduled_start_at_second} ]]; then
-          if [[ ${live_scheduled_start_at_second} -le ${limit_second} ]]; then
-            echo "| ${live_scheduled_start_at} | [${content_code}](${domain}/live/${content_code}) | ${video_allow_dvr_flg} | ${video_convert_to_vod_flg} |";
-          fi;
+      video_allow_dvr_flg="$(jq --raw-output '.video.allow_dvr_flg' <<<"${live_info}")";
+      [[ "${video_allow_dvr_flg}" == 'true' ]] && video_allow_dvr_flg='';
+
+      video_convert_to_vod_flg="$(jq --raw-output '.video.convert_to_vod_flg' <<<"${live_info}")";
+      [[ "${video_convert_to_vod_flg}" == 'true' ]] && video_convert_to_vod_flg='';
+
+      live_scheduled_start_at_second=$(date --date="${live_scheduled_start_at}" '+%s');
+
+      title="$(jq --raw-output '.title' <<<"${live_info}")";
+
+      thumbnail_url="$(jq --raw-output '.thumbnail_url' <<<"${live_info}")";
+      if [[ "${thumbnail_url}" != 'null' ]]; then
+        thumbnail_element="<img alt=\"${title}\" src=\"${thumbnail_url}\" height=\"128px\">"
+      else
+        thumbnail_element='<i>no thumbnail</i>'
+      fi;
+
+      if [[ ${now_second} -le ${live_scheduled_start_at_second} ]]; then
+        if [[ ${live_scheduled_start_at_second} -le ${limit_second} ]]; then
+          key="${live_scheduled_start_at_second} ${content_code}"
+          value="$(
+            cat <<-TABLE_ROW
+						  <tr>
+						    <td>${live_scheduled_start_at}</td>
+						    <td>
+						      <a href="${domain}/live/${content_code}">${content_code}</a>
+						      <br>
+						      ${thumbnail_element}
+						      <br>
+						      ${title}
+						    </td>
+						    <td>${video_allow_dvr_flg}</td>
+						    <td>${video_convert_to_vod_flg}</td>
+						  </tr>
+						TABLE_ROW
+          )"
+          live_timestamp_code_row_map["${key}"]="${value}"
+
+          echo -e '\t''collected' >/dev/stderr
+
+          continue
         fi;
       fi;
-    fi;
-  done | \
-    sort;
 
+      echo -e '\t''ignored' >/dev/stderr
+    fi;
+  fi;
+done < <(<"${channel_list_json}" jq --compact-output '.data.content_providers | .[]')
+
+echo "count of incoming live = ${#live_timestamp_code_row_map[@]}" >/dev/stderr
+
+
+# sort live
+
+declare -a live_timestamp_code_array
+
+while read live_timestamp_code; do
+  live_timestamp_code_array+=("${live_timestamp_code}")
+done < <(
+  for live_timestamp_code in "${!live_timestamp_code_row_map[@]}"; do
+    echo "${live_timestamp_code}"
+  done | \
+  sort -k 1
+)
+
+
+# draw table
+
+echo '<table>'
+
+cat <<'TABLE_HEADER'
+  <thead>
+    <th>START (UTC)</th>
+    <th>Thumbnail, URL & Title</th>
+    <th>allow_dvr_flg</th>
+    <th>convert_to_vod_flg</th>
+  </thead>
+TABLE_HEADER
+
+for key in "${live_timestamp_code_array[@]}"; do
+  echo "${live_timestamp_code_row_map["${key}"]}"
+done
+
+echo '</table>'
