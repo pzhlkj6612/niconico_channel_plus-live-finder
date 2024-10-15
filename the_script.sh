f539c2d7c63e04e65d7e2f3f758fd1c15713e220
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2024-10-15 08:43:54 +0800

    Really, no indent in generated HTML

--- the_script.sh
+++ the_script.sh
@@ -75,18 +75,18 @@ while read -r channel_info; do
           key="${live_scheduled_start_at_second} ${content_code}"
           value="$(
             cat <<TABLE_ROW
-						  <tr>
-						    <td>${live_scheduled_start_at}</td>
-						    <td>
-						      <a href="${domain}/live/${content_code}">${content_code}</a>
-						      <br>
-						      ${thumbnail_element}
-						      <br>
-						      ${title}
-						    </td>
-						    <td>${video_allow_dvr_flg}</td>
-						    <td>${video_convert_to_vod_flg}</td>
-						  </tr>
+              <tr>
+                <td>${live_scheduled_start_at}</td>
+                <td>
+                  <a href="${domain}/live/${content_code}">${content_code}</a>
+                  <br>
+                  ${thumbnail_element}
+                  <br>
+                  ${title}
+                </td>
+                <td>${video_allow_dvr_flg}</td>
+                <td>${video_convert_to_vod_flg}</td>
+              </tr>
 TABLE_ROW
           )"
           live_timestamp_code_row_map["${key}"]="${value}"
